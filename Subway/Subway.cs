338c9e8a6a80c79047b63fb92ec582ea3ea62171
Allen Kim <allen.s.kim2001@gmail.com>
2023-02-03 20:05:50 -0500

    copy-pasted starter code

--- Subway/Subway.cs
+++ Subway/Subway.cs
@@ -1,16 +1,36 @@
 ﻿using System;
 
-namespace Subway
+public enum Colour { RED, YELLOW, GREEN, … } // For example
+public class SubwayMap
 {
-    public class SubwayMap
+    private class Node
     {
+        public Station connection; // Adjacent station (connection)
+        public Colour line; // Colour of its subway line
+        public Node next; // Link to the next adjacent station (Node)
+        public Node() { … }
+        public Node(Station connection, Colour c, Node next) { … }
+    }
 
-    }
-    class Program
+    private class Station
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hi");
-        }
+        public string name; // Name of the subway station
+        public bool visited; // Used for the breadth-first search
+        public Node E; // Header node for a linked list of adjacent stations
+        public Station(string name) { … }
     }
+
+    private Dictionary<string, Station> S; // Dictionary of stations
+    public SubwayMap() { … }
+
+    public void InsertStation(string name) { … }
+
+    public bool RemoveStation(string name) { … }
+
+    public bool InsertConnection(string name1, string name2, Colour c) { … }
+
+    public bool RemoveConnection(string name1, string name2, Colour c) { … }
+
+    public void ShortestRoute(string name1, string name2) { … }
+
 }
