2cb03020dde0e6a1e91f7ae592fdd90edf0cc09c
Allen Kim <allen.s.kim2001@gmail.com>
2023-02-04 01:05:31 -0500

    implemented InsertStation()

--- Subway/Subway.cs
+++ Subway/Subway.cs
@@ -1,46 +1,69 @@
 ﻿using System;
+using System.Drawing;
 
 namespace A1Q1 {
     public enum Colour { RED, YELLOW, GREEN } // For example
-    public class SubwayMap
+    public class SubwaySystem
     {
-        private class Node
+        private class Node<T>
         {
-            public Station Connection { get; set; }     // Adjacent station (connection)
+            public Station<T> Connection { get; set; }     // Adjacent station (connection)
             public Colour Line { get; set; }            // Colour of its subway line
-            public Node Next { get; set; }              // Link to the next adjacent station (Node)
-            public Node(Station connection, Colour c, Node next) {
+            public Node<T> Next { get; set; }              // Link to the next adjacent station (Node)
+            public Node(Station<T> connection, Colour c, Node<T> next) {
                 Connection = connection;
                 Line = c;
                 Next = next;
             }
         }
 
-        private class Station
+        private class Station<T>
         {
             public string Name { get; set; }            // Name of the subway station
             public bool Visited { get; set; }            // Used for the breadth-first search
-            public Node E { get; set; }                 // Header node for a linked list of adjacent stations
+            public List<Node<T>> E { get; set; }                 // Header node for a linked list of adjacent stations
             public Station(string name) {
                 Name = name;
                 Visited = false;
-                //E = new Node<T>;
+                E = new List<Node<T>>();
             }
         }
 
-        //private Dictionary<string, Station> S;          // Dictionary of stations
-        //public SubwayMap() { … }
+        
+        public interface ISubwayMap<T>
+        {
+            void InsertStation(string name);
+            bool RemoveStation(string name);
+            bool InsertConnection(string name1, string name2, Color c);
+            bool RemoveConnection(string name1, string name2, Color c);
+            void ShortestRoute(string name1, string name2, Color c);
+        }
 
-        //public void InsertStation(string name) { … }
 
-        //public bool RemoveStation(string name) { … }
+        public class SubwayMap<T> : ISubwayMap<T>
+        {
+            private Dictionary<string, Station<T>> S;          // Dictionary of stations
 
-        //public bool InsertConnection(string name1, string name2, Colour c) { … }
+            public SubwayMap() {
+                S = new Dictionary<string, Station<T>>();
+            }
 
-        //public bool RemoveConnection(string name1, string name2, Colour c) { … }
+            public void InsertStation(string name) {
+                if (S.ContainsKey(name) == false)
+                {
+                    Station<T> newStation = new Station<T>(name);
+                    S.Add(name, newStation);
+                }
+            }
 
-        //public void ShortestRoute(string name1, string name2) { … }
+            public bool RemoveStation(string name) { … }
 
+            public bool InsertConnection(string name1, string name2, Colour c) { … }
+
+            public bool RemoveConnection(string name1, string name2, Colour c) { … }
+
+            public void ShortestRoute(string name1, string name2) { … }
+        }
     }
 
     class Program
