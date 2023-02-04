508edf895e87ca3d675cfaf7afc6f46bd4b96b4d
Allen Kim <allen.s.kim2001@gmail.com>
2023-02-04 14:11:33 -0500

    minor changes1

--- Subway/Subway.cs
+++ Subway/Subway.cs
@@ -5,32 +5,33 @@ namespace A1Q1 {
     public enum Colour { RED, YELLOW, GREEN } // For example
     public class SubwaySystem
     {
-        private class Node<T>
+        private class Node
         {
-            public Station<T> Connection { get; set; }     // Adjacent station (connection)
+            public Station Connection { get; set; }     // Adjacent station (connection)
             public Colour Line { get; set; }            // Colour of its subway line
-            public Node<T> Next { get; set; }              // Link to the next adjacent station (Node)
-            public Node(Station<T> connection, Colour c, Node<T> next) {
+            public Node Next { get; set; }              // Link to the next adjacent station (Node)
+            public Node(Station connection, Colour c, Node next) {
                 Connection = connection;
                 Line = c;
                 Next = next;
             }
         }
 
-        private class Station<T>
+        private class Station
         {
             public string Name { get; set; }            // Name of the subway station
             public bool Visited { get; set; }            // Used for the breadth-first search
-            public List<Node<T>> E { get; set; }                 // Header node for a linked list of adjacent stations
+            public List<Node> E { get; set; }                 // Header node for a linked list of adjacent stations
             public Station(string name) {
                 Name = name;
                 Visited = false;
-                E = new List<Node<T>>();
+                E = new List<Node>();
             }
 
             public int FindConnection(string name)      //i think this should be changed to traverse a linked list, not an array
             {
                 int i;
+                //Node *p;
                 for (i = 0; i < E.Count; i++)
                 {
                     if (E[i].Connection.Name.Equals(name))
@@ -41,7 +42,7 @@ namespace A1Q1 {
         }
 
         
-        public interface ISubwayMap<T>
+        public interface ISubwayMap
         {
             void InsertStation(string name);
             bool RemoveStation(string name);
@@ -51,18 +52,18 @@ namespace A1Q1 {
         }
 
 
-        public class SubwayMap<T> : ISubwayMap<T>
+        public class SubwayMap : ISubwayMap
         {
-            private Dictionary<string, Station<T>> S;          // Dictionary of stations
+            private Dictionary<string, Station> S;          // Dictionary of stations
 
             public SubwayMap() {
-                S = new Dictionary<string, Station<T>>();
+                S = new Dictionary<string, Station>();
             }
 
             public void InsertStation(string name) {
                 if (S.ContainsKey(name) == false)
                 {
-                    Station<T> newStation = new Station<T>(name);
+                    Station newStation = new Station(name);
                     S.Add(name, newStation);
                 }
             }
@@ -71,13 +72,13 @@ namespace A1Q1 {
 
             public bool InsertConnection(string stationName1, string stationName2, Colour c) {
 
-                Node<T> n;
+                Node n;
 
                 if (S.ContainsKey(stationName1) && S.ContainsKey(stationName2)) {
                     if (S[stationName1].FindConnection(stationName2) == -1)
                     {
-                        //e = new Node<T>();
-                        //S[stationName1].E.Add(e);
+                        Node e = new Node(S[stationName2], c, null);
+                        S[stationName1].E.Add(e);
                     }
                 }
             }
